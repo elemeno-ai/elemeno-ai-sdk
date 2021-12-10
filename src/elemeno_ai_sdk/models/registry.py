570d3f84a68b28f900f5e3ee8262c69274a427ae
Lucas Bonatto Miguel <lucas@elemeno.ai>
2021-12-10 01:05:00 -0800

    Fixes to load registry model from torch different device

--- src/elemeno_ai_sdk/models/registry.py
+++ src/elemeno_ai_sdk/models/registry.py
@@ -1,13 +1,38 @@
+import os
+import torch
 import mlflow
 import mlflow.pyfunc
 from elemeno_ai_sdk.config import Configs
 
 class ModelRegistry:
 
-    def __init__(self, mlflow_tracking_url: str):
+    def __init__(self):
         cfg = Configs.instance()
         self._config = cfg
+        os.environ["AWS_ACCESS_KEY_ID"] = cfg.cos.key_id
+        os.environ["AWS_SECRET_ACCESS_KEY"] = cfg.cos.secret
+        #TODO fix this to load from config file
+        os.environ["MLFLOW_S3_IGNORE_TLS"] = "true"
+        minio_host = cfg.cos.host
+        os.environ["MLFLOW_S3_ENDPOINT_URL"] = f"http://{minio_host}"
         mlflow.set_tracking_uri(cfg.registry.tracking_url)
+
+
+    def get_latest_model_torch(self, model_name: str, device: str):
+        """Loads the most recent model registered and in stage Production
+        from the model registry. Returns an mlflow model object.
+
+        Keyword arguments:
+        model_name -- the name of the model in the registry
+        """
+        stage = 'Production'
+
+        model = mlflow.pytorch.load_model(
+            model_uri=f"models:/{model_name}/{stage}",
+            map_location=torch.device(device)
+        )
+
+        return model
 
     def get_latest_model(self, model_name: str):
         """Loads the most recent model registered and in stage Production
@@ -19,7 +44,7 @@ class ModelRegistry:
         stage = 'Production'
 
         model = mlflow.pyfunc.load_model(
-            model_uri=f"models:/{model_name}/{stage}"
+            model_uri=f"models:/{model_name}/{stage}",
         )
 
         return model
