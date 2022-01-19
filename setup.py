f903eb0edddd9e45b1d2d1f3c05e57f6b1e7dddf
lucasbmiguel <lucas@elemeno.ai>
2022-01-18 18:15:26 -0800

    Bump version: 0.0.43 → 0.0.44

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def parse_requirements(filename):
 
 setup(
     name='elemeno-ai-sdk',
-    version='0.0.43',
+    version='0.0.44',
     license='Apache-2.0',
     description='A set of glue code and utilities to make using elemeno AI platform a smooth experience',
     long_description='%s\n%s' % (
