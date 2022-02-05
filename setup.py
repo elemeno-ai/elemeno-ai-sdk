6d25b3dbb36bfc0657e644839f602f97d5081477
h0ffmann <hoffmann@poli.ufrj.br>
2022-02-05 14:55:18 -0300

    rm local cfg
    Signed-off-by: h0ffmann <hoffmann@poli.ufrj.br>

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def parse_requirements(filename):
 
 setup(
     name='elemeno-ai-sdk',
-    version='local',
+    version='0.0.52',
     license='Apache-2.0',
     description='A set of glue code and utilities to make using elemeno AI platform a smooth experience',
     long_description='%s\n%s' % (
