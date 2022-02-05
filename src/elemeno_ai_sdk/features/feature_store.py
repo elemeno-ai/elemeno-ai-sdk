2e02988d04a845e3426e547a99e27b52224abce3
h0ffmann <hoffmann@poli.ufrj.br>
2022-02-05 14:47:43 -0300

    add rs interfaces
    Signed-off-by: h0ffmann <hoffmann@poli.ufrj.br>

--- src/elemeno_ai_sdk/features/feature_store.py
+++ src/elemeno_ai_sdk/features/feature_store.py
@@ -10,6 +10,7 @@ from feast.protos.feast.types.Value_pb2 import Value as ValueProto
 from feast.repo_config import RepoConfig
 import pandas as pd
 from datetime import datetime
+from sqlalchemy import create_engine
 
 class BaseFeatureStore(metaclass=abc.ABCMeta):
 
@@ -20,58 +21,63 @@ class BaseFeatureStore(metaclass=abc.ABCMeta):
             hasattr(subclass, 'get_historical_features') and
             callable(subclass.get_historical_features) and
             hasattr(subclass, 'get_online_features') and
-            callable(subclass.get_online_features) and 
+            callable(subclass.get_online_features) and
             hasattr(subclass, 'apply') and
             callable(subclass.apply) and
             hasattr(subclass, 'config'))
-    
+
     def ingest(self, ft: feast.FeatureView, df: pd.DataFrame):
         pass
 
     def get_historical_features(self, entity_source: pd.DataFrame, feature_refs: typing.List[str]) -> RetrievalJob:
         pass
-    
-    def get_online_features(self, entities: typing.List[typing.Dict[str, typing.Any]], 
+
+    def get_online_features(self, entities: typing.List[typing.Dict[str, typing.Any]],
             requested_features: typing.Optional[typing.List[str]]=None) \
             -> feast.online_response.OnlineResponse:
         pass
 
-    def apply(self, objects: typing.Union[feast.Entity, feast.FeatureView, feast.OnDemandFeatureView, feast.FeatureService, 
-        typing.List[typing.Union[feast.FeatureView, feast.OnDemandFeatureView, feast.Entity, feast.FeatureService]]], 
-            objects_to_delete: typing.List[typing.Union[feast.FeatureView, feast.OnDemandFeatureView, feast.Entity, feast.FeatureService, None]] = None, 
+    def apply(self, objects: typing.Union[feast.Entity, feast.FeatureView, feast.OnDemandFeatureView, feast.FeatureService,
+        typing.List[typing.Union[feast.FeatureView, feast.OnDemandFeatureView, feast.Entity, feast.FeatureService]]],
+            objects_to_delete: typing.List[typing.Union[feast.FeatureView, feast.OnDemandFeatureView, feast.Entity, feast.FeatureService, None]] = None,
             partial: bool = True) -> None:
         pass
 
 BaseFeatureStore.register
-class FeatureStore: 
+class FeatureStore:
     def __init__(self) -> None:
         """
         FeatureStore is a BigQuery compatible Feature Store implementation
         """
         self.fs = feast.FeatureStore(repo_path=".")
         self.config = self.fs.config
-    
+
     def ingest(self, ft: feast.FeatureView, df: pd.DataFrame, schema: typing.List[typing.Dict] = None):
         project_id = self.fs.config.offline_store.project_id
         dataset = self.fs.config.offline_store.dataset
         location = self.fs.config.offline_store.location
-        df.to_gbq(destination_table=f"{dataset}.{ft.name}", 
+        df.to_gbq(destination_table=f"{dataset}.{ft.name}",
             project_id=project_id, if_exists="append", location=location, table_schema=schema)
-        
+
+    def ingest_rs(self, ft: feast.FeatureView, df: pd.DataFrame, conn_str: str):
+      conn = create_engine(conn_str)
+      df.to_sql(f"{ft.name}",
+                conn, index=False, if_exists='replace')
+
     def get_historical_features(self, entity_source: pd.DataFrame, feature_refs: typing.List[str]) -> RetrievalJob:
         return self.fs.get_historical_features(entity_source, feature_refs)
-    
-    def get_online_features(self, entities: typing.List[typing.Dict[str, typing.Any]], 
+
+    def get_online_features(self, entities: typing.List[typing.Dict[str, typing.Any]],
             requested_features: typing.Optional[typing.List[str]]=None) \
             -> feast.online_response.OnlineResponse:
         if self.fs.config.online_store == None:
             raise ValueError("Online store is not configure, make sure to configure the property online_store in the config yaml")
         return self.fs.get_online_features(features=requested_features, entity_rows=entities)
-    
-    def apply(self, objects: typing.Union[feast.Entity, feast.FeatureView, feast.OnDemandFeatureView, feast.FeatureService, 
-        typing.List[typing.Union[feast.FeatureView, feast.OnDemandFeatureView, feast.Entity, feast.FeatureService]]], 
-            objects_to_delete: typing.List[typing.Union[feast.FeatureView, feast.OnDemandFeatureView, feast.Entity, feast.FeatureService, None]] = None, 
+
+    def apply(self, objects: typing.Union[feast.Entity, feast.FeatureView, feast.OnDemandFeatureView, feast.FeatureService,
+        typing.List[typing.Union[feast.FeatureView, feast.OnDemandFeatureView, feast.Entity, feast.FeatureService]]],
+            objects_to_delete: typing.List[typing.Union[feast.FeatureView, feast.OnDemandFeatureView, feast.Entity, feast.FeatureService, None]] = None,
             partial: bool = True):
         self.fs.apply(objects=objects, objects_to_delete=objects_to_delete, partial=partial)
-        
-        
+
+
