6e069075a72fa3809aa4e890c468ae52970735df
lucasbmiguel <lucas@elemeno.ai>
2022-02-16 14:40:57 -0800

    Adding option to config file to specify feast config path

--- src/elemeno_ai_sdk/features/materialize.py
+++ src/elemeno_ai_sdk/features/materialize.py
@@ -1,13 +1,16 @@
 from datetime import datetime
 from feast.feature_store import FeatureStore
 from feast import utils
+from elemeno_ai_sdk.config import Configs
 
 DATETIME_ISO = "%Y-%m-%dT%H:%M:%s"
 
+elm_config = Configs.instance()
+
 def materialize_incremental(feature_view: str,
-                            repo_path: str,
                             end_ts=datetime.now().replace(microsecond=0).isoformat()):
-  store = FeatureStore(repo_path)
+  
+  store = FeatureStore(elm_config.feature_store.feast_config_path)
   store.materialize_incremental(
     feature_views=[feature_view],
     end_date=utils.make_tzaware(datetime.fromisoformat(end_ts)),
