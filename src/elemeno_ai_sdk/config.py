72c6924c0da7a18c507916c173ca689a775b5d34
lucasbmiguel <lucas@elemeno.ai>
2022-03-31 21:43:19 -0700

    adding code to do not fail when config file is not found

--- src/elemeno_ai_sdk/config.py
+++ src/elemeno_ai_sdk/config.py
@@ -1,3 +1,4 @@
+import logging
 from omegaconf import OmegaConf
 import os
 
@@ -11,10 +12,14 @@ class Configs:
     @classmethod
     def instance(cls):
         cfg_path = os.getenv('ELEMENO_CFG_PATH', 'elemeno.yaml')
-        if cls._instance is None:
-            cls._instance = cls.__new__(cls)
-            cls._props = OmegaConf.load(cfg_path)
-        return cls._instance.props
+        try:
+            if cls._instance is None:
+                cls._instance = cls.__new__(cls)
+                cls._props = OmegaConf.load(cfg_path)
+            return cls._instance.props
+        except:
+            logging.info("Couldn't find a config file at %s, will continue without loading it", cfg_path)
+            return None
 
     @property
     def props(self):
