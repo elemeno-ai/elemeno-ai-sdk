f6fcac984475e1c85ca5ac12e5eaa64a23a8df3c
lucasbmiguel <lucas@elemeno.ai>
2022-04-29 22:57:22 -0700

    feat: adding additional methods to minio client

--- src/elemeno_ai_sdk/cos/minio.py
+++ src/elemeno_ai_sdk/cos/minio.py
@@ -1,3 +1,4 @@
+from typing import Any
 from minio import Minio
 from minio.error import S3Error
 from elemeno_ai_sdk.config import Configs
@@ -13,10 +14,22 @@ class MinioClient:
             secret_key=cfg.cos.secret,
             secure=cfg.cos.use_ssl
         )
+    
+    def list_dir(self, bucket_name: str, prefix: str = "") -> list:
+        found = self.client.bucket_exists(bucket_name)
+        if not found:
+            raise ValueError("The specified bucket doesn't exist")
+        return self.client.list_objects(bucket_name, prefix=prefix)
 
     def get_object(self, bucket_name: str, object_path: str, destination_path: str):
         found = self.client.bucket_exists(bucket_name)
         if not found:
             raise ValueError("The specified bucket doesn't exist")
         self.client.fget_object(bucket_name, object_path, destination_path)
+
+    def put_object(self, bucket_name: str, destination_path: str, object_stream: Any, length: Any):
+        found = self.client.bucket_exists(bucket_name)
+        if not found:
+            raise ValueError("The specified bucket doesn't exist")
+        self.client.put_object(bucket_name, destination_path, object_stream, length)
 
