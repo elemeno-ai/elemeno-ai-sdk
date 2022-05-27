0116a540c7c497535d79d746d8caf7a184d9f27f
lucasbmiguel <lucas@elemeno.ai>
2022-05-27 16:25:01 -0700

    fix: adding _source as the root field for elastic queries

--- src/elemeno_ai_sdk/ml/features/ingest/source/elastic.py
+++ src/elemeno_ai_sdk/ml/features/ingest/source/elastic.py
@@ -15,11 +15,13 @@ class ElasticIngestion(BaseSource):
       res = self._es.search(index=index, query=query)
       if not 'hits' in res or not 'hits' in res['hits']:
         raise Exception("No hits found")
-      return pd.DataFrame(res['hits']['hits'])
+      sources = [hit['_source'] for hit in res['hits']['hits']]
+      return pd.DataFrame(sources)
     all_results = []
     pages = count // max_per_page + 1
     for page in range(1, pages):
       res = self._es.search(index=index, query=query, size=100, from_=page*100)
       if 'hits' in res and 'hits' in res['hits']:
-        all_results.extend(res['hits']['hits'])
+        sources = [hit['_source'] for hit in res['hits']['hits']]
+        all_results.extend(sources)
     return pd.DataFrame(all_results)
