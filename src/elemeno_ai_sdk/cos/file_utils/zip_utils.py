713b3cd7bc1eebdf4f7cd9b5250d84a899650ae8
lucasbmiguel <lucas@elemeno.ai>
2022-06-21 02:51:47 -0700

    fixes on zip utils

--- src/elemeno_ai_sdk/cos/file_utils/zip_utils.py
+++ src/elemeno_ai_sdk/cos/file_utils/zip_utils.py
@@ -24,13 +24,13 @@ class ZipUtils:
     if not os.path.exists(folder_path):
       raise ValueError("The folder path doesn't exist")
     with ZipFile(dest_filename, "w") as zip_file:
-      src = glob.glob('actor_critic_buybox/**/*', recursive=True)
+      src = glob.glob(f'{folder_path}/**/*', recursive=True)
       if excludes is not None:
         src = [s for s in src if s not in excludes]
       _ = [zip_file.write(f) for f in src]
     return dest_filename
 
-  def unzip_file(self, zip_file_path: str, dest_folder_path: str) -> str:
+  def unzip_file(self, zip_file_path: str, dest_folder_path: Optional[str] = None) -> str:
     """
     Unzip a file.
      - param zip_file_path: The path to the zip file.
@@ -38,7 +38,7 @@ class ZipUtils:
     Returns:
       The path to the folder.
     """
-    if not os.path.exists(zip_file_path):
+    if dest_folder_path is not None and not os.path.exists(zip_file_path):
       raise ValueError("The zip file path doesn't exist")
     with ZipFile(zip_file_path, "r") as zip_file:
       zip_file.extractall(dest_folder_path)
