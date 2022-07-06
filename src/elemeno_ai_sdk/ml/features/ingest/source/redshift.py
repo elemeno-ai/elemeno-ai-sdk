7ea8e5b5a3f26022b2350dcd663c213db1a3c29d
lucasbmiguel <lucas@elemeno.ai>
2022-07-06 12:31:49 -0700

    adding more docs and some refactorings

---
+++ src/elemeno_ai_sdk/ml/features/ingest/source/redshift.py
@@ -0,0 +1,48 @@
+from typing import Optional
+import pandas as pd
+
+import redshift_connector
+
+from elemeno_ai_sdk.ml.features.ingest.source.base_source import BaseSource
+
+MAX_LINES_TO_IMPORT = 10000
+
+
+# TODO Add sql metadata validation
+class RedshiftIngestionSource(BaseSource):
+
+    def __init__(self, database: str, cluster_name: Optional[str]=None, base_query: Optional[str]=None,
+      host: Optional[str]=None, port: Optional[int]=None, user: Optional[str]=None, password: Optional[str]=None):
+        if cluster_name is not None and (host is not None or port is not None or user is not None or password is not None):
+          raise ValueError("When specifying cluster name you cannot specify host, port, user or password")
+        self.host = host
+        self.port = port
+        self.user = user
+        self.password = password
+        self.cluster_name = cluster_name
+        self.base_query = base_query
+        self.database = database
+
+    def read(self, base_query: Optional[str]=None, **kwargs) -> pd.DataFrame:
+      if base_query is not None:
+        self.base_query = base_query
+      with redshift_connector.connect(
+          iam=True,
+          database=self.database,
+          db_user='awsuser',
+          password='',
+          user='',
+          cluster_identifier=self.cluster_name,
+          profile='default'
+      ) as conn:
+        with conn.cursor() as cursor:
+          conn.autocommit = True
+          cursor.execute(" {} ".format(self.base_query))
+          return cursor.fetch_dataframe()
+
+    def read_after(self, timestamp_str: str, base_query: Optional[str] = None, **kwargs) -> pd.DataFrame:
+      if base_query is not None:
+        self.base_query = base_query
+      self.base_query = " {} WHERE event_timestamp > '{}' ORDER BY event_timestamp ASC LIMIT {}; " \
+          .format(self.base_query, timestamp_str, MAX_LINES_TO_IMPORT)
+      return self.read()
