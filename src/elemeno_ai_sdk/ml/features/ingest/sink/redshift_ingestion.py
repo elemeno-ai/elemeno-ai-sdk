efa61cc2e8627943a174ec96354287ab5b349ff6
bruno <oliveira.ufrj11@gmail.com>
2022-07-14 14:33:19 -0300

    Add new status column when the user has a media column in the to_ingest dataframe

--- src/elemeno_ai_sdk/ml/features/ingest/sink/redshift_ingestion.py
+++ src/elemeno_ai_sdk/ml/features/ingest/sink/redshift_ingestion.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import json
 import typing
+from xmlrpc.client import Boolean
 import feast
 from sqlalchemy import VARCHAR, create_engine
 from sqlalchemy.sql import text
@@ -11,6 +12,7 @@ from elemeno_ai_sdk.ml.features.feature_table import FeatureTable
 from elemeno_ai_sdk.ml.features.types import FeatureType
 from elemeno_ai_sdk import logger
 from elemeno_ai_sdk.ml.features.ingest.sink.base_ingestion import Ingestion
+from elemeno_ai_sdk.cos.minio import MinioClient
 
 MAP_TYPES = {
   ''
@@ -71,14 +73,14 @@ class RedshiftIngestion(Ingestion):
           chunk = to_ingest.iloc[i * max_rows_per_insert:(i + 1) * max_rows_per_insert]
         chunk.to_sql(f"{ft.name}",
                 conn, index=False, if_exists='append',
-                method='multi', 
+                method='multi',
                 chunksize=1000)
     except Exception as exception:
       logger.error("Failed to ingest data to Redshift: %e", exception)
       raise exception
     finally:
       conn.dispose()
-  
+
   def create_table(self, to_ingest: pd.DataFrame, ft: FeatureTable, engine: sqlalchemy.engine.Engine) -> pd.DataFrame:
     """
     Creates a table in Redshift if it does not exist.
@@ -98,6 +100,9 @@ class RedshiftIngestion(Ingestion):
     date_cols = ["created_timestamp", "create_timestamp", "event_timestamp", "created_date", "record_date", "updated_date"]
     columns = {}
     for col, dtype in to_ingest.dtypes.items():
+      if col == 'media':
+          columns[f'{col}_status'] = 'BOOLEAN'
+          to_ingest[f'{col}_status'] = False
       if col in date_cols:
         columns[col] = "TIMESTAMP"
       elif dtype == "object":
@@ -121,7 +126,7 @@ class RedshiftIngestion(Ingestion):
     create = create[:-1] + ")"
     engine.execute(create)
     return to_ingest
-  
+
   def ingest_schema(self, feature_table: FeatureTable, schema_file_path: str) -> None:
     """
     This method should be called if you want to use a jsonschema file to create the feature table
@@ -140,14 +145,13 @@ class RedshiftIngestion(Ingestion):
         adjusted_dtypes = {}
         dummy_row = {}
         for name, prop in jschema["properties"].items():
-          
           fmt = prop["format"] if "format" in prop else None
           if prop["type"] == "string" and "size" in prop:
             adjusted_dtypes[name] = VARCHAR(int(prop["size"]))
           table_schema.append({"name": name, "type": FeatureType.from_str_to_bq_type(prop["type"], format=fmt).name})
           this_pd_type = FeatureType.from_str_to_pd_type(prop["type"], format=fmt)
           pd_schema[name] = pd.Series(dtype=this_pd_type)
-          dummy_row[name] = FeatureType.get_dummy_value(this_pd_type)
+          dummy_row[name] = FeatureType.get_dummy_value(this_pd_type) # we don't need to create this dict 
           if "isKey" in prop and prop["isKey"] == "true":
             feature_table.register_entity(feast.Entity(name=name, description=name, value_type=FeatureType.from_str_to_feature_type(prop["type"])))
           else:
@@ -164,12 +168,12 @@ class RedshiftIngestion(Ingestion):
 
         logger.info("FT types schema: %s", table_schema)
         feature_table.set_table_schema(table_schema)
-        conn = create_engine(self._conn_str, hide_parameters=True, isolation_level="AUTOCOMMIT")
         dummy_df = pd.DataFrame(pd_schema)
         if len(adjusted_dtypes) == 0:
           adjusted_dtypes = None
-        dummy_df = dummy_df.append(dummy_row, ignore_index=True)
+        dummy_df = dummy_df.append(dummy_row, ignore_index=True) # we don't need to append the dummy row here
         #TODO Bruno - When there's any column with type binary_download in the schema, create an auxiliary feature_table with the list of files to download for each entity
+        conn = create_engine(self._conn_str, hide_parameters=True, isolation_level="AUTOCOMMIT")
         self.create_table(dummy_df, feature_table, conn)
         # dummy_df.to_sql(f"{feature_table.name}",
         #         conn, index=False, if_exists='append', dtype=adjusted_dtypes)
