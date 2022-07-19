c105041a00ec7f640ab07dd72f6254b94a47f87a
lucasbmiguel <lucas@elemeno.ai>
2022-07-18 17:58:39 -0700

    adding unit tests for elastic

--- tests/features/ingest/source/elastic.py
+++ tests/features/ingest/source/elastic.py
@@ -1,8 +1,11 @@
+import os
 import mock
 from mock import patch
 import pytest
+from elemeno_ai_sdk.ml.features.feature_store import FeatureStore
 
 from elemeno_ai_sdk.ml.features.ingest.source.elastic import ElasticIngestionSource
+from elemeno_ai_sdk.ml.features.ingest.source.ingestion_source_builder import IngestionSourceType
 
 @patch('elasticsearch.Elasticsearch')
 @pytest.fixture
@@ -11,6 +14,13 @@ def mocked_elastic():
         mock.patch("elasticsearch.client.IndicesClient.create") as mocked_index_create:
     mocked_search.return_value = "pipopapu"
     mocked_index_create.return_value = {"acknowledged": True}
+
+@pytest.fixture
+def feature_store():
+  os.environ["ELEMENO_CFG_FILE"] = "./elemeno.yaml.TEMPLATE"
+  with mock.patch("elemeno_ai_sdk.ml.features.ingest.source.ingestion_source_builder.IngestionSourceBuilder.build_elastic"):
+    f = FeatureStore(sink_type=None, source_type=IngestionSourceType.ELASTIC)
+    return f
 
 def test_read_happy_path(mocked_elastic):
   source = ElasticIngestionSource("http://localhost:9200", "username", "password")
@@ -23,3 +33,9 @@ def test_read_happy_path(mocked_elastic):
     assert len(result) == 2
     assert result.iloc[0]["id"] == "1"
     assert result.iloc[1]["id"] == "2"
+
+def test_read_from_feature_store(mocked_elastic, feature_store):
+  query = {"query": {"match_all": {}}}
+  r = feature_store.read_and_ingest_from_query(query, 10, index="test-index")
+  print(r)
+  pass
