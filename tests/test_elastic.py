2e6503f8c2149aa8c2c82353c2058d6907f907c8
lucasbmiguel <lucas@elemeno.ai>
2022-07-19 19:22:08 -0700

    upda ting

--- tests/test_elastic.py
+++ tests/test_elastic.py
@@ -4,7 +4,7 @@ from elemeno_ai_sdk.ml.features.ingest.sink.ingestion_sink_builder import Ingest
 from elemeno_ai_sdk.ml.features.ingest.source.elastic import ElasticIngestion
 
 def test_elastic_ingestion():
-    conn_str = "postgresql://USER:PASS@***REMOVED***:5439/***REMOVED***"
+    conn_str = "postgresql://USER:PASS@redshift-cluster-2.cwuptesab6o8.us-east-1.redshift.amazonaws.com:5439/lucas-elemeno-ai"
     fs = FeatureStore(sink_type=IngestionSinkType.REDSHIFT, connection_string=conn_str)
     ft = FeatureTable(name="test_5", feature_store=fs)
 
