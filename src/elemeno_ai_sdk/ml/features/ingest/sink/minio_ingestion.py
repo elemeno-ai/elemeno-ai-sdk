fbce6a681743d61348a21d7bf3bc4630b91f3b50
lucasbmiguel <lucas@elemeno.ai>
2022-08-04 20:48:37 -0700

    adjustments to download files

--- src/elemeno_ai_sdk/ml/features/ingest/sink/minio_ingestion.py
+++ src/elemeno_ai_sdk/ml/features/ingest/sink/minio_ingestion.py
@@ -3,6 +3,7 @@ import io
 import numpy as np
 import os
 from typing import Dict, List, Optional, Tuple
+import dask.array as da
 from dask.distributed import Client
 import requests
 from elemeno_ai_sdk.config import Configs
@@ -14,40 +15,40 @@ def install():
   import sys
   os.system("pip install minio")
 
-def io_batch_dask(params: 'IngestionParams'):
+def io_batch_dask(params: List['IngestionParams']):
   from elemeno_ai_sdk.cos.minio import MinioClient
   from elemeno_ai_sdk.config import logging
   
   logging.error("Started batch dask")
-  p = params
-  client = MinioClient(host=p.minio_host,
-    access_key=p.minio_user,
-    secret_key=p.minio_pass,
-    use_ssl=p.minio_ssl)
-  headers = {"user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36"}
-  to_ingest = p.to_ingest
-  media_id = to_ingest[p.media_id_col]
-  media_url = to_ingest[p.media_url_col].replace("\\", "")
-  folder_id = to_ingest[p.dest_folder_col]
-  position = to_ingest['position']
-  media_url = media_url.replace('/{description}.', "/x.")
+  for p in params:
+    client = MinioClient(host=p.minio_host,
+      access_key=p.minio_user,
+      secret_key=p.minio_pass,
+      use_ssl=p.minio_ssl)
+    headers = {"user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36"}
+    to_ingest = p.to_ingest
+    media_id = to_ingest[p.media_id_col]
+    media_url = to_ingest[p.media_url_col].replace("\\", "")
+    folder_id = to_ingest[p.dest_folder_col]
+    position = to_ingest['position']
+    media_url = media_url.replace('/{description}.', "/x.")
 
-  r = requests.get(media_url, headers=headers, stream=True)
-  content_type = r.headers['content-type']
-  logging.error("Will check image")
-  if content_type.startswith('image'):
-      logging.error("Image found")
-      st = io.BytesIO(r.content)
-      media_extension = media_url.split('.')[-1]
-      try:
-          client.put_object('elemeno-cos', f"binary_data_parallel/{folder_id}/{position}_{media_id}.{media_extension}", st)
-          logging.error("uploaded path: " + f"binary_data_parallel/{folder_id}/{position}_{media_id}.{media_extension}")
-      except Exception as e:
-          logging.error("error uploading file to folder: " + folder_id)
-          logging.error(e)
-  else:
-    logging.error(f'{media_id}: {media_url}')
-    logging.error("Not an image")
+    r = requests.get(media_url, headers=headers, stream=True)
+    content_type = r.headers['content-type']
+    logging.error("Will check image")
+    if content_type.startswith('image'):
+        logging.error("Image found")
+        st = io.BytesIO(r.content)
+        media_extension = media_url.split('.')[-1]
+        try:
+            client.put_object('elemeno-cos', f"binary_data_parallel/{folder_id}/{position}_{media_id}.{media_extension}", st)
+            logging.error("uploaded path: " + f"binary_data_parallel/{folder_id}/{position}_{media_id}.{media_extension}")
+        except Exception as e:
+            logging.error("error uploading file to folder: " + folder_id)
+            logging.error(e)
+    else:
+      logging.error(f'{media_id}: {media_url}')
+      logging.error("Not an image")
   return True
 
 class IngestionParams:
@@ -83,8 +84,10 @@ class MinioIngestionDask(FileIngestion):
         config.feature_store.source.params.binary.dest_folder_col,
         to_ingest=d)
       params.append(i)
+    arr = da.from_array(params, chunks=1000)
     print("Client will map to scheduler")
-    m = self.dask_client.map(io_batch_dask, params, batch_size=10000)
-    print("Will compute")
-    self.dask_client.compute(m)
-    print("Finished dask processing result")
+    print("len params: " + str(len(params)))
+    big_future = self.dask_client.scatter(arr)
+    future = self.dask_client.submit(io_batch_dask, big_future)
+    future.result()
+    return None
