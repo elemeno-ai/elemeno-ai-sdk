4fd5af7541af02a21ad804381d631ac2486ce333
oliveirabruno92 <oliveira.ufrj11@gmail.com>
2023-08-09 10:40:49 -0300

    Add automl client

--- src/elemeno_ai_sdk/ml/automl/client.py
+++ src/elemeno_ai_sdk/ml/automl/client.py
@@ -1,4 +1,4 @@
-from typing import Any
+import logging
 from typing import Dict
 
 import aiohttp
@@ -16,46 +16,72 @@ class AutoMLClient:
 
     @property
     def base_url(self):
-        return self.URL[self.env]
+        try:
+            return self.URL[self.env]
+        except KeyError:
+            logging.exception("Invalid environment. Please use dev or prod.")
 
     @property
     def headers(self):
         return {"Content-Type": "application/json", "x-api-key": self.api_key}
 
     async def list_jobs(self):
-        async with aiohttp.ClientSession(
-            base_url=self.base_url, headers=self.headers
-        ) as session:
-            response = await session.get(url="/automl")
+        async with aiohttp.ClientSession(headers=self.headers) as session:
+            async with session.get(url=f"{self.base_url}/automl") as response:
+                if not response.ok:
+                    logging.exception(
+                        f"Failed to start automl job with: \n"
+                        f"\t status code= {response.status} \n"
+                        f"\t header= {self.headers}"
+                    )
+                return await response.json()
 
-            if response.status != 200:
-                print(f"Failed to list automl jobs with status code: {response.status}")
+    async def run_job(
+        self,
+        experiment_id: str,
+        feature_table_name: str,
+        id_column: str,
+        target_name: str,
+        start_date: str,
+        end_date: str,
+        task: str,
+        scoring: str,
+        num_features: int,
+        generations: int,
+    ) -> Dict[str, str]:
+        body = {
+            "experimentID": experiment_id,
+            "featureTableName": feature_table_name,
+            "idColumn": id_column,
+            "targetName": target_name,
+            "startDate": start_date,
+            "endDate": end_date,
+            "task": task,
+            "scoring": scoring,
+            "numFeatures": num_features,
+            "generations": generations,
+        }
+        async with aiohttp.ClientSession(headers=self.headers) as session:
+            async with session.post(
+                url=f"{self.base_url}/automl", json=body
+            ) as response:
+                if not response.ok:
+                    logging.exception(
+                        f"Failed to start automl job with: \n"
+                        f"\t status code= {response.status} \n"
+                        f"\t message_body= {body} \n"
+                        f"\t header= {self.headers}"
+                    )
 
-            data = await response.json()
-            return data
-
-    async def run_job(self, data: Dict[str, Any]) -> Dict[str, str]:
-        async with aiohttp.ClientSession(
-            base_url=self.base_url, headers=self.headers
-        ) as session:
-            response = await session.post(url="/automl", data=data)
-
-            if response.status != 200:
-                print(f"Failed to start automl job with status code: {response.status}")
-
-            data = await response.json()
-            return data
+                return await response.json()
 
     async def get_job(self, job_id: str):
-        async with aiohttp.ClientSession(
-            base_url=self.base_url, headers=self.headers
-        ) as session:
-            response = await session.get(url=f"/automl/{job_id}")
-
-            if response.status != 200:
-                print(
-                    f"Failed to get automl job with job_id {job_id} and status code: {response.status}"
-                )
-
-            data = await response.json()
-            return data
+        async with aiohttp.ClientSession(headers=self.headers) as session:
+            async with session.get(url=f"{self.base_url}/automl/{job_id}") as response:
+                if not response.ok:
+                    logging.exception(
+                        f"Failed to start automl job with: \n"
+                        f"\t status code= {response.status} \n"
+                        f"\t header= {self.headers}"
+                    )
+                return await response.json()
