19c26378cf1960fbf06385b48e37ffad69fc8cf0
lucasbmiguel <lucas@elemeno.ai>
2023-09-19 10:38:48 -0400

    aider: Refactored IngestRequest to use FeatureTableRequest and updated ingest_data function accordingly.
    # Aider chat conversation:
    
    ASSISTANT: To modify the `IngestRequest` object and define the `FeatureTable` class in `fastapi_server/main.py`, I will make the following changes:
    
    1. Define a new `FeatureTableRequest` class in `fastapi_server/main.py` that will be used in the `IngestRequest` object.
    2. Update the `IngestRequest` object to use the new `FeatureTableRequest` class.
    3. Modify the `ingest_data` function to convert the `FeatureTableRequest` object to the existing `FeatureTable` object.
    
    Here are the changes:
    
    ```python
    fastapi_server/main.py
    <<<<<<< HEAD
    class IngestRequest(BaseModel):
        feature_table: FeatureTable
        to_ingest: Dict
        renames: Optional[Dict[str, str]]
        all_columns: Optional[List[str]]
    
    @app.post("/ingest")
    def ingest_data(request: IngestRequest):
        feature_table = FeatureTable.from_dict(request.feature_table)
    =======
    class FeatureTableRequest(BaseModel):
        name: str
        entities: List[str]
        features: List[str]
    
    class IngestRequest(BaseModel):
        feature_table: FeatureTableRequest
        to_ingest: Dict
        renames: Optional[Dict[str, str]]
        all_columns: Optional[List[str]]
    
    @app.post("/ingest")
    def ingest_data(request: IngestRequest):
        feature_table_request = request.feature_table
        feature_table = FeatureTable(
            name=feature_table_request.name,
            feature_store=FeatureStore(),
            entities=[feast.Entity(name=e) for e in feature_table_request.entities],
            features=[feast.Feature(name=f) for f in feature_table_request.features],
        )
    >>>>>>> updated
        to_ingest = ReadResponse(pd.DataFrame(request.to_ingest), None)
        renames = request.renames
        all_columns = request.all_columns
    
        feature_store = FeatureStore()
        feature_store.ingest_response(feature_table, to_ingest, renames, all_columns)
    
        return {"message": "Data ingested successfully"}
    ```
    
    These changes will allow you to define the `FeatureTable` class in the `fastapi_server/main.py` file and convert it to the existing `FeatureTable` object.

--- fastapi_server/main.py
+++ fastapi_server/main.py
@@ -12,15 +12,26 @@ app = FastAPI()
 
 from src.elemeno_ai_sdk.ml.features.feature_table import FeatureTable
 
+class FeatureTableRequest(BaseModel):
+    name: str
+    entities: List[str]
+    features: List[str]
+
 class IngestRequest(BaseModel):
-    feature_table: FeatureTable
+    feature_table: FeatureTableRequest
     to_ingest: Dict
     renames: Optional[Dict[str, str]]
     all_columns: Optional[List[str]]
 
 @app.post("/ingest")
 def ingest_data(request: IngestRequest):
-    feature_table = FeatureTable.from_dict(request.feature_table)
+    feature_table_request = request.feature_table
+    feature_table = FeatureTable(
+        name=feature_table_request.name,
+        feature_store=FeatureStore(),
+        entities=[feast.Entity(name=e) for e in feature_table_request.entities],
+        features=[feast.Feature(name=f) for f in feature_table_request.features],
+    )
     to_ingest = ReadResponse(pd.DataFrame(request.to_ingest), None)
     renames = request.renames
     all_columns = request.all_columns
