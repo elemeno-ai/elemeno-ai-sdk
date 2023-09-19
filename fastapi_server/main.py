de49365fe0b7e98ea3ea9e90c4e25c6594c76810
lucasbmiguel <lucas@elemeno.ai>
2023-09-19 10:06:50 -0400

    aider: Implemented FastAPI server for feature store ingest method
    # Aider chat conversation:
    
    USER: Create a new folder with a server implementation in FastAPI for the feature store ingest method
    
    ASSISTANT: I will create a new folder named `fastapi_server` and a new file named `main.py` inside it with a FastAPI server implementation for the feature store ingest method.
    
    ```python
    fastapi_server/main.py
    <<<<<<< HEAD
    =======
    from fastapi import FastAPI, Body
    from pydantic import BaseModel
    from typing import Dict, List, Optional
    
    from src.elemeno_ai_sdk.ml.features.feature_store import FeatureStore
    from src.elemeno_ai_sdk.ml.features.feature_table import FeatureTable
    from src.elemeno_ai_sdk.ml.features.ingest.source.base_source import ReadResponse
    
    app = FastAPI()
    
    class IngestRequest(BaseModel):
        feature_table: Dict
        to_ingest: Dict
        renames: Optional[Dict[str, str]]
        all_columns: Optional[List[str]]
    
    @app.post("/ingest")
    def ingest_data(request: IngestRequest):
        feature_table = FeatureTable.from_dict(request.feature_table)
        to_ingest = ReadResponse(pd.DataFrame(request.to_ingest), None)
        renames = request.renames
        all_columns = request.all_columns
    
        feature_store = FeatureStore()
        feature_store.ingest_response(feature_table, to_ingest, renames, all_columns)
    
        return {"message": "Data ingested successfully"}
    
    if __name__ == "__main__":
        import uvicorn
        uvicorn.run(app, host="0.0.0.0", port=8000)
    >>>>>>> updated
    ```
    
    This FastAPI server implementation has an endpoint `/ingest` that accepts a POST request with the required data for the feature store ingest method. The server will run on port 8000.

---
+++ fastapi_server/main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI, Body
+from pydantic import BaseModel
+from typing import Dict, List, Optional
+
+from src.elemeno_ai_sdk.ml.features.feature_store import FeatureStore
+from src.elemeno_ai_sdk.ml.features.feature_table import FeatureTable
+from src.elemeno_ai_sdk.ml.features.ingest.source.base_source import ReadResponse
+
+app = FastAPI()
+
+class IngestRequest(BaseModel):
+    feature_table: Dict
+    to_ingest: Dict
+    renames: Optional[Dict[str, str]]
+    all_columns: Optional[List[str]]
+
+@app.post("/ingest")
+def ingest_data(request: IngestRequest):
+    feature_table = FeatureTable.from_dict(request.feature_table)
+    to_ingest = ReadResponse(pd.DataFrame(request.to_ingest), None)
+    renames = request.renames
+    all_columns = request.all_columns
+
+    feature_store = FeatureStore()
+    feature_store.ingest_response(feature_table, to_ingest, renames, all_columns)
+
+    return {"message": "Data ingested successfully"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
