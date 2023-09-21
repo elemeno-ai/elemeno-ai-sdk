c133b07f99496d55b552b18944c64f15005f4187
lucasbmiguel <lucas@elemeno.ai>
2023-09-21 12:24:06 -0400

    feat: wip replacing the feature store to use remote server

--- fastapi_server/main.py
+++ fastapi_server/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Body, Depends
 from fastapi.security import APIKeyHeader
+import feast
 import pandas as pd
 from pydantic import BaseModel
 from typing import Dict, List, Optional
@@ -14,30 +15,26 @@ from src.elemeno_ai_sdk.ml.features.feature_table import FeatureTable
 
 class FeatureTableRequest(BaseModel):
     name: str
-    entities: List[str]
-    features: List[str]
 
 class IngestRequest(BaseModel):
     feature_table: FeatureTableRequest
-    to_ingest: Dict
+    to_ingest: List[Dict]
     renames: Optional[Dict[str, str]]
     all_columns: Optional[List[str]]
 
 @app.post("/ingest")
 def ingest_data(request: IngestRequest):
+    feature_store = FeatureStore()
     feature_table_request = request.feature_table
     feature_table = FeatureTable(
         name=feature_table_request.name,
-        feature_store=FeatureStore(),
-        entities=[feast.Entity(name=e) for e in feature_table_request.entities],
-        features=[feast.Feature(name=f) for f in feature_table_request.features],
+        feature_store=feature_store._fs
     )
     to_ingest = ReadResponse(pd.DataFrame(request.to_ingest), None)
     renames = request.renames
     all_columns = request.all_columns
 
-    feature_store = FeatureStore()
-    feature_store.ingest_response(feature_table, to_ingest, renames, all_columns)
+    feature_store.ingest(feature_table, to_ingest, renames, all_columns)
 
     return {"message": "Data ingested successfully"}
 
