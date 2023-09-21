8b3c7a12e328c5511816b5bde0e5797794d5f627
lucasbmiguel <lucas@elemeno.ai>
2023-09-21 16:26:55 -0400

    fix: a

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def parse_requirements(filename):
 
 setup(
     name="elemeno-ai-sdk",
-    version="0.6.2.beta.beta",
+    version="0.6.2.beta",
     license="Apache-2.0",
     description="A set of glue code and utilities to make using elemeno AI platform a smooth experience",
     long_description="%s\n%s"
