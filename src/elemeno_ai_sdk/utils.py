037a1d09ded8017850d21706c2f5cc1e500bc8a6
oliveirabruno92 <oliveira.ufrj11@gmail.com>
2023-09-28 11:36:37 -0300

    Add method to create feature table

--- src/elemeno_ai_sdk/utils.py
+++ src/elemeno_ai_sdk/utils.py
@@ -1,40 +1,14 @@
-import os
-import aiohttp
 import logging
-from typing import Callable, Optional
+import os
 from functools import wraps
+from typing import Callable, Optional
 
-PROD_URL = "https://c3po.ml.semantixhub.com"
-DEV_URL = "https://c3po-stg.ml.semantixhub.com"
+import aiohttp
 
-class MLHubRemote:
-
-    def __init__(self, env: Optional[str] = None):
-        if env:
-            self._env = env
-
-    @property
-    def env(self):
-        return self._env if hasattr(self, "_env") else os.getenv("MLHUB_ENV", "prod")
-    
-    @env.setter
-    def set_env(self, env: str):
-        self._env = env
-
-    @property
-    def base_url(self):
-        base_url = None
-        if self.env == "prod":
-            base_url = PROD_URL
-        elif self.env == "dev":
-            base_url = DEV_URL
-        else:
-            raise ValueError("Invalid environment. Please use dev or prod.")
-        return base_url
 
 def mlhub_auth(func: Callable):
     @wraps(func)
-    async def wrapper(self, *args, session: aiohttp.ClientSession = None, **kwargs):
+    async def wrapper(self, *args, session: Optional[aiohttp.ClientSession] = None, **kwargs):
         if session is None:
             api_key = os.getenv("MLHUB_API_KEY")
             if api_key is None:
@@ -44,7 +18,10 @@ def mlhub_auth(func: Callable):
                 kwargs["session"] = session
                 return await func(self, *args, **kwargs)
         else:
-            logging.warning("Calling a method anottated with with_auth and passing a session object is not recommended in production.")
+            logging.warning(
+                "Calling a method anottated with with_auth and passing a session object is not recommended in production."
+            )
             kwargs["session"] = session
             return await func(self, *args, **kwargs)
+
     return wrapper
