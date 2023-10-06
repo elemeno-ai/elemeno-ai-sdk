0bbef95d34cadc8b6ae7667bfbc7b2366e91977c
oliveirabruno92 <oliveira.ufrj11@gmail.com>
2023-10-06 11:21:13 -0300

    Add initial refactor

--- src/elemeno_ai_sdk/ml/features/feature_table.py
+++ src/elemeno_ai_sdk/ml/features/feature_table.py
@@ -1,11 +1,7 @@
-import datetime
+import json
 from typing import Any, Dict, List
 
-import feast
-import feast.types
-from feast.data_format import JsonFormat
-
-from elemeno_ai_sdk.ml.features.types import FeatureType
+from elemeno_ai_sdk.ml.mlhub_client import MLHubRemote
 
 
 class FeatureTable:
@@ -14,195 +10,60 @@ class FeatureTable:
     If you're looking to create a new feature table or read data look at ingest_schema of the class FeatureStore.
     """
 
-    def __init__(
-        self,
-        name: str,
-        feature_store: feast.FeatureStore = None,
-        entities: List[feast.Entity] = None,
-        features: List[feast.Feature] = None,
-        ttl_duration_weeks=52,
-        is_streaming=False,
-        online=False,
-        event_column: str = "event_timestamp",
-        created_column: str = "created_timestamp",
-    ):
-        self.name = name
-        self._entities = [] if entities is None else entities
-        self._features = [] if features is None else features
-        self._feast_elm = feature_store
-        self._duration = ttl_duration_weeks
-        self._online = online
-        self.is_streaming = is_streaming
-        self._evt_col = event_column
-        self._created_col = created_column
-        self._original_schema = None
-        self._table_schema = []
+    def __init__(self, remote_server: str, schema_path: str, timestamp_col: str = "event_timestamp"):
+        self._mlhub_client = MLHubRemote()
+        self._remote_server = remote_server
+        self.schema_path = schema_path
+        self.timestamp_col = timestamp_col
 
     @property
-    def entities(self) -> List[feast.Entity]:
-        return self._entities
+    def ft_name(self) -> str:
+        return self._table_schema.get("name")
 
     @property
-    def evt_col(self):
-        return self._evt_col
+    def entities(self) -> List[str]:
+        return self._table_schema.get("entities")
+
+    def _validate_schema(self, table_schema: Dict[str, Any]):
+        features = [feat["name"] for feat in table_schema["schema"]]
+        if self.timestamp_col not in features:
+            raise ValueError(f"{self.timestamp_col} must be in the table schema with type timestamp")
 
     @property
-    def created_col(self):
-        return self._created_col
+    def table_schema(self) -> Dict[str, Any]:
+        with open(self.schema_path, "r") as schema:
+            table_schema = json.load(schema)
 
-    @property
-    def table_schema(self) -> List[Dict]:
-        return self._table_schema
+        self._validate_schema(table_schema)
 
-    @property
-    def original_schema(self) -> Dict[str, Any]:
-        return self._original_schema
+        return table_schema
 
     @entities.setter
-    def entities(self, value):
-        self._entities = value
+    def entities(self, entities: List[str]):
+        self._entities = entities
 
     @property
     def features(self):
-        return self._features
+        return self._table_schema.get("schema")
 
     @features.setter
-    def features(self, value):
-        self._features = value
+    def features(self, features: List[Dict[str, Any]]):
+        self._features = features
 
-    def set_table_schema(self, value: List[Dict]) -> None:
-        self._table_schema = value
+    def set_table_schema(self, table_schema: Dict[str, Any]) -> None:
+        self._table_schema = table_schema
 
-    def set_original_schema(self, value: List[Dict]) -> None:
-        self._original_schema = value
+    async def create(self) -> None:
+        endpoint = f"{self._remote_server}/feature-view"
 
-    def register_entities(self, *entities: feast.Entity) -> None:
-        self.entities.extend(list(entities))
+        body = {"name": self.ft_name, "entities": self.entities, "schema": self.features}
+        return await self._mlhub_client.post(url=endpoint, body=body)
 
-    def register_features(self, *features: feast.Field) -> None:
-        self.features.extend(list(features))
+    async def list(self) -> List[Dict[str, Any]]:
+        endpoint = f"{self._remote_server}/list-feature-views"
+        response = await self._mlhub_client.get(url=endpoint)
+        return response["feature_views"]
 
-    def register_entity(self, entity: feast.Entity) -> None:
-        self.entities.append(entity)
-
-    def register_feature(self, feature: feast.Field) -> None:
-        self.features.append(feature)
-
-    def all_columns(self) -> List[str]:
-        cols = []
-        for e in self._entities:
-            cols.append(e.name)
-        for f in self._features:
-            cols.append(f.name)
-        cols.append(self._evt_col)
-        cols.append(self._created_col)
-        return cols
-
-    def _get_ft(self):
-        dataset = self._feast_elm.config.offline_store.dataset
-        to_apply = []
-        ft_source = feast.BigQuerySource(
-            table_ref=f"{dataset}.{self.name}",
-            event_timestamp_column=self.evt_col,
-            created_timestamp_column=self.created_col,
-        )
-        fv = feast.FeatureView(
-            name=self.name + "_stream", entities=self._entities, online=self._online, source=ft_source, tags={}
-        )
-        to_apply.append(fv)
-
-        if self.is_streaming:
-            ft_kafka_source = feast.KafkaSource(
-                kafka_bootstrap_servers=self._elm_config.feature_store.stream_source.params.bootstrap_servers,
-                topic=self._elm_config.feature_store.stream_source.params.topic,
-                timestamp_field=self.evt_col,
-                created_timestamp_column=self.created_col,
-                message_format=JsonFormat(),
-                batch_source=ft_source,
-            )
-            sfv = feast.StreamFeatureView(
-                name=self.name,
-                entities=self._entities,
-                schema=self._features,
-                online=self._online,
-                source=ft_kafka_source,
-            )
-            to_apply.append(sfv)
-
-        self._feast_elm.apply(objects=self.entities)
-        self._feast_elm.apply(objects=to_apply)
-        return fv
-
-    def _get_ft_rs(self):
-        to_apply = []
-        ft_source = feast.RedshiftSource(
-            name=self.name,
-            table=f"{self.name}",
-            created_timestamp_column=self.created_col,
-            timestamp_field=self.evt_col,
-        )
-        fv = feast.FeatureView(name=self.name, entities=self._entities, online=self._online, source=ft_source, tags={})
-        to_apply.append(fv)
-
-        if self.is_streaming:
-            ft_kafka_source = feast.KafkaSource(
-                name=self.name + "_stream",
-                kafka_bootstrap_servers=self._elm_config.feature_store.stream_source.params.bootstrap_servers,
-                topic=self._elm_config.feature_store.stream_source.params.topic,
-                timestamp_field=self.evt_col,
-                created_timestamp_column=self.created_col,
-                message_format=JsonFormat(
-                    schema_json=", ".join(
-                        [
-                            str(field["name"] + " " + FeatureType.from_feast_to_json_schema_feast(field["type"]))
-                            for field in self.table_schema
-                        ]
-                    )
-                ),
-                batch_source=ft_source,
-                watermark_delay_threshold=datetime.timedelta(hours=72),
-            )
-            sfv = feast.StreamFeatureView(
-                name=self.name + "_stream",
-                entities=self._entities,
-                schema=self._features,
-                online=self._online,
-                timestamp_field=self.evt_col,
-                source=ft_kafka_source,
-            )
-            to_apply.append(sfv)
-        self._feast_elm.apply(objects=self.entities)
-        self._feast_elm.apply(objects=to_apply)
-
-        return fv
-
-    def _get_ft_file(self):
-        ft_source = feast.FileSource(
-            path=f"data/{self.name}", event_timestamp_column=self.evt_col, created_timestamp_column=self.created_col
-        )
-
-        fv = feast.FeatureView(
-            name=self.name,
-            entities=self._entities,
-            schema=[feast.Field(name=f.name, dtype=feast.types.from_value_type(f.dtype)) for f in self._features],
-            online=self._online,
-            source=ft_source,
-            tags={},
-        )
-        self._feast_elm.apply(objects=self.entities)
-        self._feast_elm.apply(objects=fv)
-
-        return fv
-
-    def get_view(self) -> feast.FeatureView:
-        offline_store_type = type(self._feast_elm.fs.config.offline_store).__name__
-        if offline_store_type == "BigQueryOfflineStoreConfig":
-            f = self._get_ft()
-        elif offline_store_type == "RedshiftOfflineStoreConfig":
-            f = self._get_ft_rs()
-        elif offline_store_type == "FileOfflineStoreConfig":
-            f = self._get_ft_file()
-        else:
-            print(offline_store_type)
-            raise ValueError("Unsupported offline store type")
-        return f
+    async def delete(self):
+        endpoint = f"{self._remote_server}/{self.ft_name}/list-feature-views"
+        return await self._mlhub_client.post(url=endpoint)
