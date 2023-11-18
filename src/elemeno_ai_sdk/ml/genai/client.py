f5f9e8677e42acbc446ca6e19d9f23644d9a32ca
Wagner Rupp <wagner.rupp@semantix.ai>
2023-11-18 18:16:11 -0300

    Reorganized the GenAI client in hierarchical order

--- src/elemeno_ai_sdk/ml/genai/client.py
+++ src/elemeno_ai_sdk/ml/genai/client.py
@@ -2,142 +2,136 @@ from typing import Any, Dict, Optional, List, Union, BinaryIO, Tuple
 import io
 import os
 
-import aiohttp
 import pandas as pd
 import requests
 
-from elemeno_ai_sdk.utils import mlhub_auth
 from elemeno_ai_sdk.ml.mlhub_client import MLHubRemote
 
-class GenAI(MLHubRemote):
-    GENERATIVE_ROUTE = "/generative/project"
+class InferenceService:
+    
+    def __init__(self, service_id, api_key: str):
+        self._service_id = service_id
+        self.base_url = f"http://infserv-{self._service_id}.app.elemeno.ai"
 
-    def __init__(self, env: Optional[str] = None) -> None:
-        super().__init__(env=env)
-        self.url = f"{self.base_url}/generative"
+        self._api_key = api_key
+        self.auth_header = {
+            "authorization": f"Bearer {api_key}",
+            "x-api-key": api_key,  
+            }
 
-        api_key = os.environ["MLHUB_API_KEY"]
-        header = {"authorization": f"Bearer {api_key}"}
-        self.auth_header = header
 
-    def list_projects(self):
-        url = f"{self.base_url}/generative/project"
-        return requests.get(url=url, headers=self.auth_header)
-
-    def list_servers(self, project_id:str):
+    def start(self):
         raise NotImplementedError
 
-    # TODO: ver os efeitos do run_id no servidor
-    def deploy_server(self, name:str, project_id:str, run_id:str = None):
-        url = f"{self.base_url}/generative/project/{project_id}/model-deploy"
+    def stop(self):
+        raise NotImplementedError
+
+
+    def infer(
+            self,
+            data:Dict[str,str] = None, 
+            files:Dict[str, BinaryIO] = None, 
+            route = "/v0/inference"
+            ):
+        url = f"{self.base_url}{route}"
+        return requests.post(url, data=data, files=files, headers=self.auth_header)
+
+class Project:
+
+    def __init__(self, base_url: str, api_key:str, project_id:str) -> None:
+        self.base_url = base_url
+        self._api_key = api_key
+        self.auth_header = {"x-api-key": self._api_key} 
+        self._project_id = project_id
+
+    def create(self, name:str):
+        url = f"{self.base_url}/{self._project_id}/model-deploy"
         body = {"name": name, "checkpointPath": ""}
-        if run_id is not None:
-            body["runID"] = run_id
         return requests.post(url=url, data=body, headers=self.auth_header)
 
-    def start_server(self, server_id:str):
+    def get(self):
+        url = f"{self.base_url}/{self._project_id}/model-deploy?offset=0"
+        return requests.get(url=url, headers=self.auth_header).json()["resources"]
+
+    def delete(self):
         raise NotImplementedError
-
-    def stop_server(self):
-        raise NotImplementedError
-
-    #TODO: Get a non streamed version of status
-    def server_status(self, server_ids: Union[str, List[str]]):
-        raise NotImplementedError
-        ### url = f"{self.base_url}/inference_server/status"
-
-        ### if isinstance(server_ids, str):
-            ### server_ids = [server_ids]
-        ### return requests.post(
-            ### url=url,
-            ### data={"inferenceServerIDS":server_ids},
-            ### headers=self.auth_header)
-
-    def request_server(
-            self,
-            server_id:str,
-            body:Dict[str,str] = None, 
-            file:Dict[str, BinaryIO] = None, 
-            route = "v0/inference"
-            ):
-        url = f"http://infserv-{server_id}.app.elemeno.ai/{route}"
-        headers = self.auth_header | {'accept': 'application/json'}
-        return requests.post(url=url, data=body, files=file, headers=headers)
-
-    @mlhub_auth
-    async def get(
-        self,
-        url: str,
-        params: Optional[Dict[str, Any]] = None,
-        is_binary: bool = False,
-        session: Optional[aiohttp.ClientSession] = None,
-    ):
-        async with session.get(url=url, params=params) as response:
-            if is_binary:
-                return await response.content.read()
-
-            return await response.json(content_type=response.content_type)
+    
+    def services(self) -> Dict[str, InferenceService]:
+        service_ids = [resource["id"] for resource in self.get()]
+        return {service_id: InferenceService(service_id=service_id, api_key=self._api_key) for service_id in service_ids}
 
 
-class AutoFeatures(GenAI):
+class Resources:
+
+    def __init__(self, base_url, api_key:str):
+        self.url = f"{base_url}/project"
+        self._api_key = api_key
+        self.auth_header = {
+            "authorization": f"Bearer {api_key}",
+            "x-api-key": api_key,  
+            }
+
+    def create(self, name:str, model_type: str):
+        data = {"name": name, "modelID": model_type} 
+        return requests.post(url=self.url, data=data, headers=self.auth_header)
+
+    def get(self):
+        return requests.get(url=self.url, headers=self.auth_header).json()["resources"]
+
+    def delete(self, project_id:str):
+        url = f"{self.url}/{project_id}"
+        return requests.delete(url=url, headers=self.auth_header).json()
+
+    def projects(self) -> Dict[str, Project]:
+        project_ids = [resource["id"] for resource in self.get()]
+        return {project_id: Project(self.url, api_key=self._api_key, project_id=project_id) for project_id in project_ids}
+
+
+
+class GenAI(MLHubRemote):
+
+    def __init__(self, api_key: str = None, env: Optional[str] = None,) -> None:
+        self.url = f"{self.base_url}/generative"
+
+        self._api_key = api_key if api_key else os.environ["MLHUB_API_KEY"]
+        assert self._api_key
+        self.auth_header = {
+            "authorization": f"Bearer {self._api_key}",
+            "x-api-key": self._api_key,  
+            }
+        self.resources = Resources(base_url=self.url, api_key=self._api_key)
+    
+    def supported_models(self):
+        url = f"{self.url}/supported-model"
+        response = requests.get(url=url, headers=self.auth_header).json()
+        return [resource["id"] for resource in response["resources"]]
+
+    def status(self, service_ids):
+        url = f"{self.base_url}/inference_server/status"
+        headers = self.auth_header
+        
+        if isinstance(service_ids, str):
+            service_ids = [service_ids]
+        data = {"inferenceServerIDS": service_ids}
+
+        with requests.post(url, json=data, headers=headers, stream=True, allow_redirects=True) as response:
+            print(response.json())
+
+class AutoFeatures(InferenceService):
 
     def featurize(
             self,
-            server_id: str,
-            filename: str,            
+            data: str = None, 
+            filename: str = None,
+            route: str = "csv/?filename=df.csv",
             ) -> Dict:
-        """
-        Featurize the data in the specified CSV file by sending it to a auto-features service.
-        The function sends the specified CSV file to the remote server identified by 'server_id'
-        and retrieves the featurized data along with additional information and logs.
 
-        Parameters:
-        - server_id (str): The unique identifier of the remote server where the featurization will be performed.
-        - filename (str): The path to the CSV file containing the data to be featurized.
-
-        Returns:
-        - Dict: the server response in dictionary form. 
-
-        Example:
-        ```
-        server_id = "50498772a344a63346c1223d5"
-        filename = "path/to/data.csv"
-        client = AutoFeatures(env="dev")
-        response = client.featurize(server_id, filename)
-        data, output, logs = client.parse(reponse)
-        print("Featurized Data:", data)
-        print("Output:", output)
-        print("Logs:", logs)
-        ```
-        """
-        route = "csv/?filename=df.csv"
-        body = {"history": ''}
+        data = {"history": ''}
         with open(filename, "rb") as bfile:
             files = {"file": (filename, bfile)}
-            response = self.request_server(server_id, body, files, route)
+            response = self.infer(data=data, files=files, route=route)
         return response.json() 
     
     def parse(self, response) -> Tuple[pd.DataFrame, str, List[str]]:
-        """
-        Helper function to parse the server response into DataFrame, output and logs
-
-        Returns:
-        Tuple: A tuple containing the featurization results.
-            - df (pd.Dataframe): The featurized dataframe obtained from the server.
-            - output (str): Additional output or information related to the featurization process.
-            - logs (str): Logs or messages generated during the featurization process.
-
-        Example:
-        ```
-        server_id = "50498772a344a63346c1223d5"
-        filename = "path/to/data.csv"
-        client = AutoFeatures(env="dev")
-        response = client.featurize(server_id, filename)
-        data, output, logs = client.parse(reponse)
-        print("Featurized Data:", data)
-        print("Output:", output)
-        print("Logs:", logs)
-        ```
-        """
         df = pd.read_json(io.StringIO(response["data"]))
         return df, response["output"], response["logs"]
