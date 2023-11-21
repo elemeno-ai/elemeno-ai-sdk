6d08de422ad46d9cc2547ba843832fa68cc09897
Wagner Rupp <wagner.rupp@semantix.ai>
2023-11-21 20:47:55 -0300

    Changed calls and added example notebook

--- src/elemeno_ai_sdk/ml/genai/client.py
+++ src/elemeno_ai_sdk/ml/genai/client.py
@@ -5,7 +5,7 @@ import os
 import pandas as pd
 import requests
 
-from elemeno_ai_sdk.ml.mlhub_client import MLHubRemote
+from elemeno_ai_sdk.ml.mlhub_client import MLHubRemote, DEV_URL, PROD_URL
 
 class InferenceService:
     
@@ -19,13 +19,11 @@ class InferenceService:
             "x-api-key": api_key,  
             }
 
-
     def start(self):
         raise NotImplementedError
 
     def stop(self):
         raise NotImplementedError
-
 
     def infer(
             self,
@@ -35,6 +33,26 @@ class InferenceService:
             ):
         url = f"{self.base_url}{route}"
         return requests.post(url, data=data, files=files, headers=self.auth_header)
+
+
+class AutoFeatures(InferenceService):
+
+    def featurize(
+            self,
+            data: str = None, 
+            filename: str = None,
+            route: str = "/csv/?filename=df.csv",
+            ) -> Dict:
+
+        data = {"history": ''}
+        with open(filename, "rb") as bfile:
+            files = {"file": (filename, bfile)}
+            response = self.infer(data=data, files=files, route=route)
+        return response.json() 
+    
+    def parse(self, response) -> Tuple[pd.DataFrame, str, List[str]]:
+        df = pd.read_json(io.StringIO(response["data"]))
+        return df, response["output"], response["logs"]
 
 class Project:
 
@@ -47,18 +65,36 @@ class Project:
     def create(self, name:str):
         url = f"{self.base_url}/{self._project_id}/model-deploy"
         body = {"name": name, "checkpointPath": ""}
-        return requests.post(url=url, data=body, headers=self.auth_header)
-
-    def get(self):
-        url = f"{self.base_url}/{self._project_id}/model-deploy?offset=0"
-        return requests.get(url=url, headers=self.auth_header).json()["resources"]
+        return requests.post(url=url, json=body, headers=self.auth_header)
 
     def delete(self):
         raise NotImplementedError
+
+    def get(self, name:str = None, service_id:str = None) -> InferenceService:
+        assert name is not None or service_id is not None    
     
-    def services(self) -> Dict[str, InferenceService]:
-        service_ids = [resource["id"] for resource in self.get()]
-        return {service_id: InferenceService(service_id=service_id, api_key=self._api_key) for service_id in service_ids}
+        resources = self.list_services()
+        if resources is None:
+            raise ValueError(f"No services found. Please check the provided values or create a new service.")
+
+        if name is not None:
+            names = [resource["name"] for resource in resources]
+            if name not in names:
+                raise ValueError(f"Service with name {name} was not found.")
+            service_id = next(item for item in resources if item["name"] == name)
+
+        elif service_id is not None:
+            service_ids = [resource["id"] for resource in resources]
+            if not service_ids or (service_id not in service_ids):
+                raise ValueError(f"Service with id {service_id} was not found.")
+
+        # TODO: Consider passing a different class depending on the type of service
+        return InferenceService(service_id=service_id, api_key=self._api_key)
+
+    def list_services(self) -> List:
+        url = f"{self.base_url}/{self._project_id}/model-deploy?offset=0"
+        return requests.get(url=url, headers=self.auth_header).json()["resources"]
+
 
 
 class Resources:
@@ -73,24 +109,39 @@ class Resources:
 
     def create(self, name:str, model_type: str):
         data = {"name": name, "modelID": model_type} 
-        return requests.post(url=self.url, data=data, headers=self.auth_header)
-
-    def get(self):
-        return requests.get(url=self.url, headers=self.auth_header).json()["resources"]
+        return requests.post(url=self.url, json=data, headers=self.auth_header)
 
     def delete(self, project_id:str):
         url = f"{self.url}/{project_id}"
         return requests.delete(url=url, headers=self.auth_header).json()
 
-    def projects(self) -> Dict[str, Project]:
-        project_ids = [resource["id"] for resource in self.get()]
-        return {project_id: Project(self.url, api_key=self._api_key, project_id=project_id) for project_id in project_ids}
+    def get(self, name: str = None, project_id:str=None,) -> Project:
+        resources = self.list_projects()
+    
+        assert name is not None or project_id is not None    
+        if name is not None:
+            names = [resource["name"] for resource in resources]
+            if name not in names:
+                raise ValueError(f"Project with name {name} was not found.")
+            project_id = next(item for item in resources if item["name"] == name)
+
+        elif project_id is not None:
+            project_ids = [resource["id"] for resource in resources]
+            if not project_id or (project_id not in project_ids):
+                raise ValueError(f"Project with id {project_id} was not found.")
+
+        return Project(self.url, api_key=self._api_key, project_id=project_id) 
+
+    def list_projects(self):
+        return requests.get(url=self.url, headers=self.auth_header).json()["resources"]
 
 
+class GenAI:
+    
 
-class GenAI(MLHubRemote):
+    def __init__(self, api_key:str, env:str = None) -> None:
+        self.base_url = PROD_URL if env == "prod" else DEV_URL
 
-    def __init__(self, api_key: str = None, env: Optional[str] = None,) -> None:
         self.url = f"{self.base_url}/generative"
 
         self._api_key = api_key if api_key else os.environ["MLHUB_API_KEY"]
@@ -116,22 +167,3 @@ class GenAI(MLHubRemote):
 
         with requests.post(url, json=data, headers=headers, stream=True, allow_redirects=True) as response:
             print(response.json())
-
-class AutoFeatures(InferenceService):
-
-    def featurize(
-            self,
-            data: str = None, 
-            filename: str = None,
-            route: str = "csv/?filename=df.csv",
-            ) -> Dict:
-
-        data = {"history": ''}
-        with open(filename, "rb") as bfile:
-            files = {"file": (filename, bfile)}
-            response = self.infer(data=data, files=files, route=route)
-        return response.json() 
-    
-    def parse(self, response) -> Tuple[pd.DataFrame, str, List[str]]:
-        df = pd.read_json(io.StringIO(response["data"]))
-        return df, response["output"], response["logs"]
